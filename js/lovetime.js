7e07b6f6c5be5e71378ac8c29ba8e01b02efccd3
zylovelj <739799725@qq.com>
2021-07-26 16:54:54 +0800

    update

--- js/lovetime.js
+++ js/lovetime.js
@@ -13,7 +13,7 @@ function lovetime() {
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
     // 修改这里时间即可 我的是2018.11.19
-    var t1 = Date.UTC(2018, 11, 19, 00, 00, 00)
+    var t1 = Date.UTC(2020, 07, 07, 21, 21, 21)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
